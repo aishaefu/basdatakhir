671e11be5dac9b71c18f196021a561b5985ff36f
aishaefu <85338205+aishaefu@users.noreply.github.com>
2021-06-15 20:47:53 +0700

    index non user

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php 
+
+require 'functions.php'
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Snackie</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<header>
+			<div class="heading">
+				<a href="index.php"><img src="img/logo.png" alt=""></a>
+				<div class="right_header">
+					<ul class="button_guest">
+                        <input class="search" type="text" placeholder="Cari Resep">
+                            <a href="search.php"><img class="isearch" src="img/isearch.png" alt=""></a>
+                        <a href="form.php"><button class="blogin">Masuk/Daftar</button></a>
+                    </ul>
+				</div>
+			</div>
+		</header>
+
+        <!-- isi homepage -->
+        <section>
+            <div class="box">
+                <h2>Homepage</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    Ullam aut perspiciatis inventore! Commodi praesentium doloribus ratione, 
+                    voluptas laudantium at dignissimos!</p>
+            </div>
+        </section>
+        <!-- isi homepage -->
+
+        <!-- footer -->
+        <div class="about">
+                    <h2>tentang snackie</h2>
+                </div>
+                <div class="body_about">
+                    <p>Snackie adalah sebuah web untuk berbagi resep makanan ringan.
+                        <br>
+                        Pengguna dapat login untuk bisa membagikan, memilih resep favorit,
+                        serta berinteraksi dengan pengguna lainnya.
+                    </p>
+        <!-- akhir footer -->
+	</body>
+</html>
