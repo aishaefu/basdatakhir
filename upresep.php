19bd6b3e8d9b4eef83d088d80989689f88924912
aishaefu <85338205+aishaefu@users.noreply.github.com>
2021-06-16 15:31:17 +0700

    + session, logo ke index2

--- upresep.php
+++ upresep.php
@@ -1,15 +1,22 @@
+<?php 
+session_start();
+if( !isset($_SESSION["login"]) ) {
+    header("Location: form.php");
+    exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Buat Resep | Snackie</title>
 		<link rel="stylesheet" href="style.css">
-
 	</head>
 	<body>
 		<header>
 			<div class="heading">
-				<a href="index.php"><img src="img/logo.png" alt=""></a>
-
+				<a href="index2.php"><img src="img/logo.png" alt=""></a>
 				<div class="right_header">
 						<ul class="button_user">
               <input class="search" type="text" placeholder="Cari Resep">
@@ -18,17 +25,13 @@
               <a href="profil.php"> <img class="profil" src="img/profil.png" alt=""> </a>
 						</ul>
 				</div>
-
 			</div>
 		</header>
-
 		<div class="resep">
 			<h1>Ayo tulis resepmu !</h1>
-
 			<div class="resep_form">
 				<form action="recipe.php" method="post">
 					<input class="rform" type="text" placeholder="Judul Resep" name="judul">
-
 					<label class="upresep"><input type="file" value="upload">Unggah Foto</label>
 					<br>
 					<input class="rform" type="text" id="upresep" placeholder="username" name="username">
@@ -39,12 +42,9 @@
 				</form>
 			</div>
 		</div>
-
-
 		<div class="about">
 			<h2>tentang snackie</h2>
 		</div>
-
 		<div class="body_about">
 			<p>Snackie adalah sebuah web untuk berbagi resep makanan ringan.
 				<br>
@@ -52,6 +52,5 @@
 				serta berinteraksi dengan pengguna lainnya.
 			</p>
 		</div>
-
 	</body>
 </html>
